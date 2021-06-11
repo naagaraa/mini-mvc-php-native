1eb42b51e800a0814e506356bdd92ee72f8749f3
naagaraa <ekabersinar@gmail.com>
2021-06-11 21:11:26 +0700

    update welcome page

--- apps/views/Welcome.php
+++ apps/views/Welcome.php
@@ -20,6 +20,7 @@
 
 	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" href="<?= asset('css/fontfamily.css') ?>">
 
 	<link rel="stylesheet" href="<?= asset('css/style.min.css') ?>">
@@ -28,141 +29,96 @@
 	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> -->
 
 	<link rel="stylesheet" href="<?= asset('css/style.min.css') ?>">
+    <style>
+       body{background-color:#090909;color:whitesmoke}.release{display:inline-block;color:orange!important;text-decoration:none}.color-orange{color:orange!important}.release:hover{color:orangered!important}.nav-bottom{background-color:black!important}.card-custom{margin-top:12rem}.creator{font-size:12px}
+    </style>
 </head>
 
-<style>
-	.theme {
-		background-color: dark-grey;
-		margin-top: 5rem;
-	}
+<body>
+    <nav class="navbar navbar-dark nav-bottom">
+        <div class="container">
+            <a class="navbar-brand color-orange">
+               <h5> Nagara </h5>
+            </a>
+            <div class="d-flex">
+                <p>view <a href="#" class="navbar-item release">release notes</a> for mini mvc 4.0.1</p> 
+            </div>
+        </div>
+    </nav>  
+    
+    <div class="container">
+        <div class="row d-flex justify-content-center d-flex align-items-center mx-auto my-auto">
+            <div class="col-6 align-self-center">
+                <div class="card-custom text-center">
+                    <h5 class="color-orange" >The install worked successfully! Congratulations!</h5>
+                    <span>project mini mvc php native develop by eka jaya nagara as miyuki nagara, my background can't used framework popular php like  CI or Laravel for finish exam "kerja praktik", so I'm develop this is.</span>
+                    <!-- <p class="mt-3 bg-warning text-dark">'trying edit this page at apps/views/welcome.php'</p> -->
+                </div>
+            </div>
+        </div>
+    </div>
 
-	.card-list {
-		width: 11rem !important;
-		height: 11rem !important;
-		border-radius: 25px;
-		cursor: pointer;
-		font-size: small;
-	}
 
-	.nav-card {
-		display: inline-block;
-		text-decoration: none;
-		color: inherit;
-	}
+    <div class="container-fluid nav-bottom fixed-bottom ">
+        <div class="row">
+            <div class="col">
+                <p class="mt-3 creator text-start"> PHP Version <?= phpversion() ?></p>
+            </div>
+            <div class="col">
+                <p class="mt-3 creator text-end"> Copyright &copy; 2018-<?= year_now() ?> Backend By Eka Jaya Nagara</p>
+            </div>
+        </div>
+        <div class="row nav-bottom py-4 mx-5 my-4">
+            <div class="col">
+               <div class="row">
+                   <div class="col-3 d-flex align-items-center">
+                        <i class="fas fa-code align-self-center"></i>
+                   </div>
+                   <div class="col">
+                        <h5 class="color-orange" ><a href="#" class="navbar-item release">Documentation</a></h5>
+                        <span>Topics, references, & how to's</span>
+                   </div>
+               </div>
+            </div>
+            <div class="col">
+                <div class="row">
+                   <div class="col-3 d-flex align-items-center">
+                        <i class="fab fa-youtube align-self-center"></i>
+                   </div>
+                   <div class="col">
+                        <h5 class="color-orange"><a href="#" class="navbar-item release">Video : Documentation</a></h5>
+                        <span>Short documentation at youtube</span>
+                   </div>
+               </div>
+            </div>
+            <div class="col">
+                <div class="row">
+                   <div class="col-3 d-flex align-items-center">
+                        <i class="fab fa-github align-self-center"></i>
+                   </div>
+                   <div class="col">
+                        <h5 class="color-orange"><a href="#" class="navbar-item release">Start Github</a></h5>
+                        <span>source code, fork & start</span>
+                   </div>
+               </div>
+            </div>
+            <div class="col">
+                <div class="row">
+                   <div class="col-3 d-flex align-items-center">
+                        <i class="fas fa-hand-holding-usd align-self-center"></i>
+                   </div>
+                   <div class="col">
+                        <h5 class="color-orange"><a href="#" class="navbar-item release">Support</a></h5>
+                        <span>donation support indonesia only</span>
+                   </div>
+               </div>
+            </div>
+        </div>
+    </div>
 
-	.color-one:hover {
-		background-color: #7CDEDC;
-		color: white;
-	}
-
-	.color-two:hover {
-		background-color: #DEC0F1;
-		color: black;
-	}
-
-	.color-three:hover {
-		background-color: #F21B3F;
-		color: white;
-	}
-
-	.color-four:hover {
-		background-color: #FFC43D;
-		color: black;
-	}
-
-	.color-five:hover {
-		background-color: #101419;
-		color: white;
-	}
-
-	.color-six:hover {
-		background-color: #5F00BA;
-		color: white;
-	}
-
-	.color-seven:hover {
-		background-color: #08BDBD;
-		color: white;
-	}
-
-	.color-eight:hover {
-		background-color: #2541B2;
-		color: white;
-	}
-
-	.code{
-		background-color: black;
-		padding: 10px;
-		color: greenyellow;
-		font-weight: bold;
-	}
-</style>
-
-<body id="my-theme" class="theme">
-	<div class="container my-3">
-		<div class="row d-flex justify-content-center">
-			<div class="col-10">
-				<div class="row mt-5">
-					<div class="col">
-						<h1>MINI MVC PHP NATIVE</h1>
-						<p class="text-muted">project mini mvc php native develop by eka jaya nagara as miyuki nagara, my background can't used framework popular php like  CI or Laravel for finish exam "kerja praktik", so I'm develop this is.</p>
-						<span>kamu bisa mencoba Edit tampilan view ini : </span>
-						<p class="code mt-2">welcome page = apps/views/welcome.php</p>
-					</div>
-				</div>
-				<div class="row d-flex justify-content-center g-4">
-					<div class="col-md-3">
-						<a class="nav-card" href="https://www.youtube.com/channel/UCYsZhw6Mlk23Q-nUPP9t1YA">
-							<div class="card card-list border-primary color-three">
-								<div class="card-body">
-									<h5 class="card-title">Youtube</h5>
-									<h6 class="card-subtitle mb-3 text-muted">Support</h6>
-									<p class="card-text">support me on this platform</p>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3">
-						<a class="nav-card" href="https://saweria.co/naagaraa">
-							<div class="card card-list border-primary color-four">
-								<div class="card-body">
-									<h5 class="card-title">Saweria</h5>
-									<h6 class="card-subtitle mb-3 text-muted">Donation ID</h6>
-									<p class="card-text">Give me Donation on this platform</p>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3">
-						<a class="nav-card" href="http://docs.minimvcphp.nagara.my.id/">
-							<div class="card card-list border-primary color-five">
-								<div class="card-body">
-									<h5 class="card-title">Documentation</h5>
-									<h6 class="card-subtitle mb-3 text-muted">mini mvc docs</h6>
-									<p class="card-text">documentation official</p>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-md-3">
-						<a class="nav-card" href="<?= url('info') ?>">
-							<div class="card card-list border-primary color-six">
-								<div class="card-body">
-									<h5 class="card-title">PHP version</h5>
-									<h6 class="card-subtitle mb-3 text-muted">Support</h6>
-									<p class="card-text">check versi php yang terinstall</p>
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<footer class="container text-center my-4">
+	<!-- <footer class="container text-center my-4">
 		<p>Copyright &copy; 2018-<?= year_now() ?> Backend By Eka Jaya Nagara as miyuki nagara</p>
-	</footer>
+	</footer> -->
 
 	<script src="<?= asset('js/bootstrap.bundle.min.js') ?>"></script>
 </body>
